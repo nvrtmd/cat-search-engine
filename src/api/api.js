9a22ee68942b943978dcc750d02f4941c3a7dcf7
YUZAMIN <untmd.crmn@gmail.com>
2022-11-11 21:25:15 +0900

    :sparkles: API 통신으로 데이터 fetch와 에러를 처리 하는 함수 구현

--- src/api/api.js
+++ src/api/api.js
@@ -1,1 +1,17 @@
-const api = "api";
+const fetchData = async (url) => {
+  try {
+    const response = await fetch(url);
+    if (response.ok) {
+      const fetchedData = await response.json();
+      return fetchedData;
+    } else {
+      const errorData = await response.json();
+      throw errorData;
+    }
+  } catch (err) {
+    throw {
+      message: err.message,
+      status: err.status,
+    };
+  }
+};
