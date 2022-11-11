b74d2f2e993de1e50ce64ece78140431d0528172
YUZAMIN <untmd.crmn@gmail.com>
2022-11-12 01:22:33 +0900

    :sparkles: 검색어 입력 input 생성

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -4,16 +4,35 @@ export default class SearchEngine {
     this.onRandomSearch = onRandomSearch;
     this.header = document.createElement("header");
     this.header.className = "header";
-    this.header.innerHTML = "HEADER";
-
     $app.appendChild(this.header);
+    console.log("Search Engine Header Rendered!");
     this.render();
+    console.log("Search Engine Section Rendered!");
   }
   render() {
+    // createElement
+    const displayModeSection = document.createElement("section");
+    displayModeSection.className = "display-mode-section";
+    displayModeSection.innerHTML = "DISPLAY MODE!";
+
+    const searchSection = document.createElement("section");
+    searchSection.className = "search-section";
+
+    const searchInput = document.createElement("input");
+    searchInput.className = "search-input";
+    searchInput.placeholder = "검색어를 입력하세요!";
+
     const randomButton = document.createElement("button");
     randomButton.innerHTML = "RANDOM!";
 
+    // addEventLister
     randomButton.addEventListener("click", this.onRandomSearch);
-    this.header.appendChild(randomButton);
+
+    // appendChild
+    this.header.appendChild(displayModeSection);
+
+    searchSection.appendChild(searchInput);
+    searchSection.appendChild(randomButton);
+    this.header.appendChild(searchSection);
   }
 }
