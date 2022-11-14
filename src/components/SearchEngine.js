383fc1c0ec24db6ebbe70be2116de6e7721815fd
YUZAMIN <untmd.crmn@gmail.com>
2022-11-15 01:31:53 +0900

    :sparkles: dark mode 토글 버튼 기능 구현

--- src/components/SearchEngine.js
+++ src/components/SearchEngine.js
@@ -14,11 +14,38 @@ export default class SearchEngine {
     searchInput.focus();
   }
 
+  onClickDisplayModeToggleButton(e) {
+    const button = e.target;
+    button.classList.toggle("activated");
+    this.changeDisplayMode(e);
+  }
+
+  changeDisplayMode(e) {
+    const body = document.querySelector("body");
+    const displayModeToggleButton = document.querySelector(
+      ".display-mode-toggle-button"
+    );
+
+    if (e.target) {
+      if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        // dark-mode일 떄 light-mode로
+        body.classList.toggle("light-mode");
+        displayModeToggleButton.classList.toggle("light-mode");
+      } else {
+        // light-mode일 떄 dark-mode로
+        body.classList.toggle("dark-mode");
+        displayModeToggleButton.classList.toggle("dark-mode");
+      }
+    }
+  }
+
   render() {
     // createElement
-    const displayModeSection = document.createElement("section");
-    displayModeSection.className = "display-mode-section";
-    displayModeSection.innerHTML = "DISPLAY MODE!";
+    const displayModeToggleBack = document.createElement("div");
+    displayModeToggleBack.className = "display-mode-toggle-back";
+    const displayModeToggleButton = document.createElement("div");
+    displayModeToggleButton.className = "display-mode-toggle-button";
+    // displayModeToggleButton.innerHTML = "🌕";
 
     const searchSection = document.createElement("section");
     searchSection.className = "search-section";
@@ -32,6 +59,9 @@ export default class SearchEngine {
     randomButton.innerHTML = "RANDOM!";
 
     // addEventLister
+    displayModeToggleButton.addEventListener("click", (e) => {
+      this.onClickDisplayModeToggleButton(e);
+    });
     randomButton.addEventListener("click", this.onRandomSearch);
     searchInput.addEventListener("keyup", (e) => {
       if (e.key === "Enter") {
@@ -40,7 +70,8 @@ export default class SearchEngine {
     });
 
     // appendChild
-    this.header.appendChild(displayModeSection);
+    displayModeToggleBack.appendChild(displayModeToggleButton);
+    this.header.appendChild(displayModeToggleBack);
 
     searchSection.appendChild(searchInput);
     searchSection.appendChild(randomButton);
