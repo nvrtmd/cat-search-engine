d0704505ed0579667dc4fbd6116365def11aaa9c
YUZAMIN <untmd.crmn@gmail.com>
2022-12-02 21:02:17 +0900

    :sparkles: slider 컴포넌트에 이미지 배치

--- src/components/SliderSection.js
+++ src/components/SliderSection.js
@@ -3,6 +3,7 @@ import Card from "./Card.js";
 
 export default class SliderSection {
   constructor({ $app }) {
+    this.page = 0;
     this.sliderSection = document.createElement("section");
     this.sliderSection.className = "slider-section";
     $app.appendChild(this.sliderSection);
@@ -20,5 +21,25 @@ export default class SliderSection {
 
   setCatsList(data) {
     this.catsList = data;
+    console.log(this.catsList);
+    this.render();
+  }
+
+  render() {
+    if (!this.catsList.data) {
+      return;
+    }
+
+    const sliderWrapper = document.createElement("div");
+    sliderWrapper.className = "slider-wrapper";
+
+    this.catsList.data.slice(this.page, this.page + 5).map((cat) => {
+      const catImage = document.createElement("img");
+      catImage.className = "cat-image";
+      catImage.src = cat.url;
+      sliderWrapper.appendChild(catImage);
+    });
+
+    this.sliderSection.appendChild(sliderWrapper);
   }
 }
